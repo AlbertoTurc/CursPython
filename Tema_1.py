e03155598ddebfc130aad6c3cf2541b2319d9d13
albertoturc <turcalberto@yahoo.it>
2023-02-26 20:10:39 +0200

    Homework1

---
+++ Tema_1.py
@@ -0,0 +1,20 @@
+#Declara o lista
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print(my_list)
+#Ordoneaza ascendent
+my_list.sort()
+print(my_list)
+#Ordoneaza descendent
+my_list.sort(reverse=True)
+print(my_list)
+#Lista ce continte numere pare din lista ordonata
+print(my_list)
+my_slice = my_list[0:9:2]
+print(my_slice)
+#lista ce contine numere impare din lista ordonata
+print(my_list)
+my_slice_1 = my_list[1:9:2]
+print(my_slice_1)
+#multipli ai numarului 3
+my_slice_2 = my_list[1:9:3]
+print(my_slice_2)
